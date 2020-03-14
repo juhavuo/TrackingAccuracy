87ddf06d599e726b04d44c0b5c7f0af6748b0193
juhavuo <juha.vuokko@pp.inet.fi>
2020-03-14 02:28:40 +0200

    StartMappingActivity logic developed further

--- app/src/main/java/fi/metropolia/juhavuo/myapplication/StartMappingActivity.kt
+++ app/src/main/java/fi/metropolia/juhavuo/myapplication/StartMappingActivity.kt
@@ -3,6 +3,7 @@ package fi.metropolia.juhavuo.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_start_mapping.*
 
 class StartMappingActivity : AppCompatActivity() {
@@ -18,7 +19,22 @@ class StartMappingActivity : AppCompatActivity() {
 
         starting_dialog_start_button.setOnClickListener {
             if(starting_dialog_name_edittext.text.isNotEmpty() && starting_dialog_details_edittext.text.isNotEmpty()){
+                val intent = Intent(this, TrackingMapActivity::class.java)
+                startActivity(intent)
+            }else{
+                var toastText = ""
+                if(starting_dialog_name_edittext.text.isEmpty()){
+                    toastText += "Name missing"
+                }
+                if(starting_dialog_details_edittext.text.isEmpty()){
+                    if(toastText.isNotEmpty()){
+                        toastText +=", details text missing"
+                    }else{
+                        toastText += "Details text missing"
+                    }
+                }
 
+                Toast.makeText(this, toastText, Toast.LENGTH_SHORT).show()
             }
         }
     }
---
+++ app/src/main/java/fi/metropolia/juhavuo/myapplication/TrackingMapActivity.kt
@@ -0,0 +1,12 @@
+package fi.metropolia.juhavuo.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TrackingMapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tracking_map)
+    }
+}
