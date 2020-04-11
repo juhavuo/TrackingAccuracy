68f73300ad5cbc2750fb40736a0a7d4b2c63bc25
juhavuo <juha.vuokko@pp.inet.fi>
2020-04-11 15:27:26 +0300

    notification channel added

--- app/src/main/java/fi/metropolia/juhavuo/trackingaccuracy/MainActivity.kt
+++ app/src/main/java/fi/metropolia/juhavuo/trackingaccuracy/MainActivity.kt
@@ -1,7 +1,12 @@
 package fi.metropolia.juhavuo.trackingaccuracy
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,9 +16,27 @@ class MainActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        createNotificationChannel()
+
         main_new_button.setOnClickListener {
             val intent = Intent(this,StartMappingActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun createNotificationChannel(){
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelID = "NotificationChannelForLocationService"
+            val channelName = getString(R.string.notification_channel_name)
+            val notificationDescription = getString(R.string.notification_content)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val notificationChannel = NotificationChannel(channelID,channelName,importance)
+            notificationChannel.description = notificationDescription
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(notificationChannel)
+
+            Log.i("test",notificationChannel.toString())
         }
     }
 }
